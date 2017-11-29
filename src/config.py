115c3910460ff5c2a02c5c7867d74990eb14a8f7
leowan <jabberwockwanli@163.com>
2017-11-30 00:03:24 +0800

    trivial

--- src/config.py
+++ src/config.py
@@ -13,7 +13,12 @@ Date: 2017/11/27 10:41:01
 """
 
 USE_HIDDEN_LAYER = True
+USE_TOY_DATA = True
 LAYER_WIDTH = 10
 FEATURE_NUM = 5
 LEARNING_RATE = 0.01
 MODEL_PATH = "./model_ranknet.ckpt"
+SAVE_TRAIN_DATA = "./train.data.save"
+SAVE_TEST_DATA = "./test.data.save"
+LOAD_TRAIN_DATA = "./train.data"
+LOAD_TEST_DATA = "./test.data"
